ab2a2d9bfaf6b0a3dc3a4abaff46c1d71899aee4
Doren12345 <DorenLee97@gmail.com>
2024-09-10 23:31:58 +0800

    feat: now will interact with the configuration of actions. (configuration: action: 1;2;3)

--- src/main/java/org/doren/cardgame/gui/battle/battleGUI.java
+++ src/main/java/org/doren/cardgame/gui/battle/battleGUI.java
@@ -1,10 +1,6 @@
 package org.doren.cardgame.gui.battle;
 
-import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
-import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,63 +8,25 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
-import org.doren.cardgame.Utils;
+import org.bukkit.plugin.Plugin;
+import org.doren.cardgame.gui.GUI;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import static org.doren.cardgame.gui.ItemStackMarker.getItemMarker;
 
 public class battleGUI implements Listener {
     private Inventory inv;
-    private YamlConfiguration UIconfig;
+    private Plugin plugin;
 
-    public void init(YamlConfiguration UIconfig) {
-        this.UIconfig = UIconfig;
-    }
+    public battleGUI() {}
 
-    public battleGUI() {
+    public void init(Plugin plugin) {
+        inv = new GUI().generateInventoryFromConfig("battleUI");
+        this.plugin = plugin;
 
-        if (UIconfig.getKeys(false).isEmpty()) {
-            Bukkit.getLogger().warning("Configuration is not initialized.");
-            return;
-        }
-
-        inv = Bukkit.createInventory(null, 54, Utils.getLangData("gui-battle-title"));
-
-//        for (UIconfig)
-        initializeItems();
-    }
-
-    // You can call this whenever you want to put the items in
-    public void initializeItems() {
-        inv.addItem(createGuiItem(Material.DIAMOND_SWORD, "Example Sword", "§aFirst line of the lore", "§bSecond line of the lore"));
-        inv.addItem(createGuiItem(Material.IRON_HELMET, "§bExample Helmet", "§aFirst line of the lore", "§bSecond line of the lore"));
-    }
-
-    // Nice little method to create a gui item with a custom name, and description
-    protected ItemStack createGuiItem(final Material material, final String name, final String... lore) {
-        final ItemStack item = new ItemStack(material, 1);
-        final ItemMeta meta = item.getItemMeta();
-
-        // Set the name of the item
-        meta.displayName(Component.text("&7"));
-
-        // Set the lore of the item
-        List<Component> itemLoreComponent = new ArrayList<>();
-        for (String l : lore) {
-            itemLoreComponent.add(Component.text(l));
-        }
-        meta.lore(itemLoreComponent);
-
-        // apply change
-        item.setItemMeta(meta);
-
-        return item;
-    }
-
-    // You can open the inventory with this
-    public void openInventory(final HumanEntity ent) {
-        ent.openInventory(inv);
+        Bukkit.getServer().getPluginManager().registerEvents(this, this.plugin);
     }
 
     // Check for clicks on items
@@ -76,24 +34,25 @@ public class battleGUI implements Listener {
     public void onInventoryClick(final InventoryClickEvent e) {
         if (!e.getInventory().equals(inv)) return;
 
-        e.setCancelled(true);
-
         final ItemStack clickedItem = e.getCurrentItem();
+
+        String[] action = getItemMarker(clickedItem, "GUIAction").split(";");
+
+        if (Arrays.asList(action).contains("cancel")) e.setCancelled(true);
 
         // verify current item is not null
         if (clickedItem == null || clickedItem.getType().isAir()) return;
 
         final Player p = (Player) e.getWhoClicked();
 
-        // Using slots click is a best option for your inventory click's
-        p.sendMessage("You clicked at slot " + e.getRawSlot());
+        // ...
     }
 
-    // Cancel dragging in our inventory
-    @EventHandler
-    public void onInventoryClick(final InventoryDragEvent e) {
-        if (e.getInventory().equals(inv)) {
-            e.setCancelled(true);
-        }
-    }
+//    // Cancel dragging in our inventory
+//    @EventHandler
+//    public void onInventoryClick(final InventoryDragEvent e) {
+//        if (e.getInventory().equals(inv)) {
+//            e.setCancelled(true);
+//        }
+//    }
 }
